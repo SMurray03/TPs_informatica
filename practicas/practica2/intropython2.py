3981a41b52bcfd6b389846017233e68b1f6c2431
SMurray03 <santimurray2003@gmail.com>
2022-09-13 23:44:45 -0300

    Tpracticos

--- practicas/practica2/intropython2.py
+++ practicas/practica2/intropython2.py
@@ -3,11 +3,11 @@
 
 
 
-s = input("ingrese su palabra: ")
-if s[0] != s.upper():
-    print("es minuscula")
-else:
-    print("es mayuscula")
+#s = input("ingrese su palabra: ")
+#if s[0] != s.upper():
+#    print("es minuscula")
+#else:
+#    print("es mayuscula")
 
 
 
@@ -27,19 +27,19 @@ else:
     print("Es impar y positivo")
 
 #Ejercicio 3
-dado = int(input("Que numero te salio: "))
-if dado == 1:
-    print("Su opuesto es el 6")
-elif dado == 2:
-    print("El opuesto es el 5")
-elif dado == 3:
-    print("El opuesto es el 4")
-elif dado == 4:
-    print("El opuesto es el 3")
-elif dado == 5:
-    print("El opuesto es el 2")
-else:
-    print("Su opuesto es el 1")
+#dado = int(input("Que numero te salio: "))
+#if dado == 1:
+#    print("Su opuesto es el 6")
+#elif dado == 2:
+#    print("El opuesto es el 5")
+#elif dado == 3:
+#    print("El opuesto es el 4")
+#elif dado == 4:
+#    print("El opuesto es el 3")
+#elif dado == 5:
+#    print("El opuesto es el 2")
+#else:
+#    print("Su opuesto es el 1")
 
 #Ejercicio 4
 
@@ -58,3 +58,54 @@ else:
     for i in range(7):
         if i == dia_de_semana:
             print(semana[i-1])
+
+#Ejercicio 6
+#lista1 = []
+#for i in range(5):
+#    lista1.append(input("Inserte cadena: "))
+#lista2 = list(lista1)
+#lista2.reverse()
+#for e in lista2:
+#    print(e)
+
+#Ejercicio 7
+#l = []
+#for i in range():
+#    l.append(input("Ingrese su numero:"))
+#for e in l:
+#    print(l)
+
+#Ejercicio 8
+l1 = []
+for i in range(5):
+    l1.append(int(input("Ingrese sus numeros:")))
+    print(l1)
+l2 = []
+for i in range(5):
+    l2.append(int(input("Dame tus numeros:")))
+    print(l2)
+l3 = [l1[0] + l2[0], l1[1] + l2[1], l1[2] + l2[2], l1[3] + l2[3], l1[4] + l2[4]]
+print(l3)
+
+#Ejercicio 9
+
+
+
+
+#Ejercicio 10
+string = input("Inserte string: ")
+diccionario = {}
+for caracter in string:
+    diccionario[caracter] = string.count(caracter)
+print(diccionario)
+
+#Ejercicio 14
+def temperatura_media(temp_max,temp_min):
+    return (temp_max + temp_min) / 2
+
+c_de_dias = int(input("cantidad de días: "))
+
+for dia in range(cantidad_de_dias):
+    t_maxima = float(input("Temperatura maxima: "))
+    t_minima = float(input("Temperatura minima: "))
+    print("La temperatura media del día es de " + str(temperatura_media(t_maxima,t_minima)) + " grados")
